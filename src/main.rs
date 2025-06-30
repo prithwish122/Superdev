e7c6eecbddc3d805a2036242ca9863b9b4a6f19e
prithwish122 <prithwishchatterjee1277@gmail.com>
2025-06-30 22:21:25 +0530

    1st func

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{Router, routing::get, serve};
+use axum::{Router, routing::{get, post}, serve, Json};
+use serde_json::json;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
@@ -6,9 +7,21 @@ async fn handler() -> &'static str {
     "Hey this is a test"
 }
 
+async fn keypair_handler() -> Json<serde_json::Value> {
+    Json(json!({
+        "success": true,
+        "data": {
+            "pubkey": "base58-encoded-public-key",
+            "secret": "base58-encoded-secret-key"
+        }
+    }))
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/keypair", post(keypair_handler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on http://{}", addr);
